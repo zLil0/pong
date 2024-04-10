f8e54559489e060bf87f8b235a13d0b640c99cb6
zLilo <murilofm.social@gmail.com>
2024-04-10 05:01:13 -0300

    Score and  Win detection

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class Player {
     constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
+        this.score = 0
     }
     draw() {
         ctx.beginPath()
@@ -17,7 +18,9 @@ class Player {
         ctx.fillRect(this.position.x, this.position.y, Player.width, Player.height)
     }
     update() {
-        this.position.y += this.velocity.y
+        if(this.position.y + this.velocity.y >= 0 && this.position.y + Player.height + this.velocity.y <= canvas.height){
+            this.position.y += this.velocity.y
+        }
     }
 }
 
@@ -38,28 +41,36 @@ class Ball {
     }
 }
 
-const ball = new Ball({
-    position: {
-        x: 430,
-        y: 290
-    },
-    velocity: {
-        x: -5,
-        y: -1
-    }
-})
+let gameOver = false
+let ball
 
-const background = () => {
-    for (let i = 0; i < 100; i += 1) {
-        if (i % 2 !== 0) {
-            ctx.beginPath()
-            ctx.fillStyle = 'white'
-            ctx.fillRect(445, (i * 40), 10, 50)
-            ctx.closePath()
+const newBall = (player) => {
+    let vX, vY, pX
+    if(player === 'p1'){
+        vX = 10
+        pX = 460
+    }
+    else if(player === 'p2'){
+        vX = -10
+        pX = 430
+    }
+    else{
+        vX = 10
+        pX = 460
+    }
+    ball = new Ball({
+        position: {
+            x: pX,
+            y: 290
+        },
+        velocity: {
+            x: vX,
+            y: -1
         }
-
-    }
+    })
 }
+
+newBall()
 
 const p1 = new Player({
     position: {
@@ -80,6 +91,24 @@ const p2 = new Player({
     }
 })
 
+const background = () => {
+    for (let i = 0; i < 100; i += 1) {
+        if (i % 2 !== 0) {
+            ctx.beginPath()
+            ctx.fillStyle = 'white'
+            ctx.fillRect(445, (i * 40), 10, 50)
+            ctx.closePath()
+        }
+    }
+    ctx.beginPath()
+    ctx.fillStyle = 'white'
+    ctx.font = '50px arial'
+    ctx.textAlign = 'center'
+    ctx.fillText(`${p1.score}`, 370, 50)
+    ctx.fillText(`${p2.score}`, 530, 50)
+    ctx.closePath()
+
+}
 
 
 p1.draw()
@@ -95,11 +124,33 @@ const ballCollidesPlayer = () => {
 }
 
 const ballCollidesMap = () => {
-    return (ball.position.y + 5 === 0 || ball.position.y + Ball.size + 5 === canvas.height)
+    return (ball.position.y - 5 === 0 || ball.position.y + Ball.size + 5 === canvas.height)
+}
+
+const scoreDetection = () => {
+    let player
+    if(ball.position.x + Ball.size <= 0){
+        player = 'p2'
+    }
+    else if(ball.position.x >= canvas.width){
+        player = 'p1'
+    }
+    return player
+}
+
+const winDetection = () => {
+    if(p1.score === 10 || p2.score === 10){
+        if(p1.score > p2.score) document.querySelector('#win p').innerHTML = "Jogador 1 Ganhou!"
+        else document.querySelector('#win p').innerHTML = "Jogador 2 Ganhou!"
+
+        document.getElementById('win').style.display = "flex"
+        gameOver = true
+    }
 }
 
 const animate = () => {
-    window.requestAnimationFrame(animate)
+    if(!gameOver) window.requestAnimationFrame(animate)
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     background()
     ball.draw()
@@ -113,26 +164,39 @@ const animate = () => {
         ball.velocity.y *= -1
     }
 
+    if(scoreDetection() === 'p1'){
+        p1.score ++
+        newBall('p1')
+    }
+    else if(scoreDetection() === 'p2'){
+        p2.score ++
+        newBall('p2')
+    }
+    winDetection()
     p1.update()
     p2.update()
     ball.update()
 }
 animate()
 
+const restart = () => {
+    location.reload()
+}
+
 
 document.addEventListener('keydown', ({ key }) => {
     if (key === 'w') {
-        p1.velocity.y = -5
+        p1.velocity.y = -7
     }
     else if (key === 's') {
-        p1.velocity.y = 5
+        p1.velocity.y = 7
     }
 
     if (key === 'ArrowUp') {
-        p2.velocity.y = -5
+        p2.velocity.y = -7
     }
     else if (key === 'ArrowDown') {
-        p2.velocity.y = 5
+        p2.velocity.y = 7
     }
 })
 document.addEventListener('keyup', ({ key }) => {
